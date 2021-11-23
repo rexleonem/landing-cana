b8265c66a4d56640a69f68cd8da0e8701a9f973a
Rex Leonem <rexleonem1@gmail.com>
2021-11-22 21:53:19 -0800

    All Done

---
+++ assets/js/main.js
@@ -0,0 +1,140 @@
+$(document).ready(function ($) {
+	/*=========================================
+		Parallax
+	=========================================*/
+	if ($(".parallax").length) {
+		$('.parallax').jarallax();
+	}
+
+	/*=========================================
+		Smooth Scroll
+	=========================================*/
+
+	$("a[href^='#']").on('click', function (event) {
+		event.preventDefault();
+		var $anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href')).offset().top - 0
+		}, 500);
+	});
+
+	/*=========================================
+		Order Form
+	=========================================*/
+
+	if ($("#order-form").length) {
+		$("#order-form").validate({
+			errorPlacement: function(error,element) {
+                return true;
+            },
+			rules: {
+				full_name: {
+					required: true,
+					minlength: 3
+				},
+				email: {
+					required: true,
+					email: true
+				},
+				phone: {
+					required: true,
+					number: true,
+					minlength: 10
+				},
+				billing_address: {
+					required: true,
+				},
+				city: {
+					required: true,
+				},
+				state: {
+					required: true,
+				},
+				zip: {
+					required: true,
+				},
+				country: {
+					required: true,
+				}
+			},
+			submitHandler: function(form) {
+				var formData = $('#order-form').serialize();
+				$.ajax({
+					type: 'POST',
+					url: 'assets/php/order-form.php',
+					dataType: "json",
+					data: formData,
+					success: function (data) {
+						if (data.success) {
+							$('.form-status').addClass('alert alert-success');
+							$('.form-status').text('Your Message Has been Sent Successfully');
+							$('.form-status').slideDown().delay(3000).slideUp();
+							$("#free-consultation-form").trigger("reset");
+							form.submit();
+						} else {
+							$('.form-status').addClass('alert alert-danger');
+							$('.form-status').text('Error Occurred, Please Try Again');
+							$('.form-status').slideDown().delay(3000).slideUp();
+						}
+					},
+					error: function (xhr, status, error) {
+						$('.form-status').addClass('alert alert-danger');
+						$('.form-status').text('Something Went Wrong');
+						$('.form-status').slideDown().delay(3000).slideUp();
+					}
+				});
+			}
+		});
+	}
+
+	/*=========================================
+		Free Consultation Form
+	=========================================*/
+
+	if ($("#free-consultation-form").length) {
+		$('#free-consultation-form').validate({
+			errorPlacement: function(error,element) {
+                return true;
+            },
+			rules: {
+				full_name: {
+					required: true,
+					minlength: 3
+				},
+				email: {
+					required: true,
+					email: true
+				},
+			},
+			submitHandler: function(form) {
+				var formData = $('#free-consultation-form').serialize();
+				$.ajax({
+					type: 'POST',
+					url: 'assets/php/popup-form.php',
+					dataType: "json",
+					data: formData,
+					success: function (data) {
+						if (data.success) {
+							$('.form-status').addClass('alert alert-success');
+							$('.form-status').text('Your Message Has been Sent Successfully');
+							form.submit();
+							$('.form-status').slideDown().delay(3000).slideUp();
+							$("#free-consultation-form").trigger("reset");
+							window.location.href = 'https://webdevproof.com/theme-forest-demo/funnel-templates/therapist/oto.html';
+						} else {
+							$('.form-status').addClass('alert alert-danger');
+							$('.form-status').text('Error Occurred, Please Try Again');
+							$('.form-status').slideDown().delay(3000).slideUp();
+						}
+					},
+					error: function (xhr, status, error) {
+						$('.form-status').addClass('alert alert-danger');
+						$('.form-status').text('Something Went Wrong');
+						$('.form-status').slideDown().delay(3000).slideUp();
+					}
+				});
+			}
+		});
+	}
+
+});
